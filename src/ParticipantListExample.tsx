6ed5adac3322c7fb21d62fd93ed1521c352a344f
Pankaj Surti <pankaj_surti@yahoo.com>
2025-01-08 18:03:20 -0600

    CallParticipantListParticipant ADDED

--- src/ParticipantListExample.tsx
+++ src/ParticipantListExample.tsx
@@ -1,4 +1,4 @@
-import { ParticipantListParticipant, ParticipantList } from '@azure/communication-react';
+import { CallParticipantListParticipant, ParticipantListParticipant, ParticipantList } from '@azure/communication-react';
 import { Stack } from '@fluentui/react';
 import React from 'react';
 
@@ -25,11 +25,53 @@ export const DefaultChatParticipantListExample: () => JSX.Element = () => {
       isRemovable: true
     }
   ];
+  const mockParticipants2: CallParticipantListParticipant[] = [
+    {
+      userId: 'user1',
+      displayName: 'You',
+      state: 'Connected',
+      isMuted: true,
+      isScreenSharing: false,
+      isRemovable: true
+    },
+    {
+      userId: 'user2',
+      displayName: 'Hal Jordan',
+      state: 'Connected',
+      isMuted: true,
+      isScreenSharing: true,
+      isRemovable: true
+    },
+    {
+      userId: 'user3',
+      displayName: 'Barry Allen',
+      state: 'Idle',
+      isMuted: false,
+      isScreenSharing: false,
+      isRemovable: true,
+      raisedHand: { raisedHandOrderPosition: 1 }
+    },
+    {
+      userId: 'user4',
+      displayName: 'Bruce Wayne',
+      state: 'Connecting',
+      isMuted: false,
+      isScreenSharing: false,
+      isRemovable: false
+    }
+  ];
 
   return (
-    <Stack>
-      <div style={{ fontSize: '1.5rem', marginBottom: '1rem', fontFamily: 'Segoe UI' }}>Participants</div>
-      <ParticipantList participants={mockParticipants} />
+    <Stack horizontalAlign="center" verticalAlign="center" verticalFill>
+        <Stack >
+        <div style={{ fontSize: '1.5rem', marginBottom: '1rem', fontFamily: 'Segoe UI' }}>Participants</div>
+        <ParticipantList participants={mockParticipants} />
+        </Stack>
+        <Stack>
+            <div style={{ fontSize: '1.5rem', marginBottom: '1rem', fontFamily: 'Segoe UI' }}>Participants</div>
+            <ParticipantList participants={mockParticipants} myUserId={'user1'} />
+        </Stack>
+
     </Stack>
   );
 };
